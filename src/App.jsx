41980ad4e45329fb6775d55f7c5759cf815e53e6
admin <vennapusaani1629@gmail.com>
2025-02-26 13:39:33 -0800

    single error

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Use Routes instead of Switch
 import Home from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
-import Single from "./components/navpage1/single";
+import Single from "./components/navpage1/Single";
 import Bulkk from "./components/navpage2/Bulkk";
 import Models from "./components/navpage3/Models";
 
