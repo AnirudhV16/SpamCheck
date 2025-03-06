2da1f9ec2b54e037556d7b8b78d84af5f9ee238c
admin <vennapusaani1629@gmail.com>
2025-03-06 12:09:33 -0800

    changes

--- ml_api/urls.py
+++ ml_api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from.views import classify_text, bulk_classify
+from ml_api.views import classify_text, bulk_classify
 
 urlpatterns = [
     path('admin/', admin.site.urls),
