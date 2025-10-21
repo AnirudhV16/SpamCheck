c31b7722254a3b365f1f8776124192d3dd0cf034
AnirudhV16 <99013548+AnirudhV16@users.noreply.github.com>
2025-10-21 19:23:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,1 @@
-#!/usr/bin/env bash 
-#Exit on ۱۲۳۵۳ 
-set -o errexit 
-#Modify this line as needed for your package manager (pip, poetry, etc.1 
-pip install -r requirements.txt 
-#Convert static asset files 
-python manage.py collectstatic --no-input 
-#Apply any outstanding database migrations 
-python manage.py migrate
+
