b00e6aa069b9a359c55a2019676ba3fbb4707f0e
Anirudh <vennapusaani1629@gmail.com>
2025-10-22 06:53:49 +0530

    shift from django to Gradio

--- ml_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from ml_api.views import classify_text, bulk_classify
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('classify/', classify_text, name='classify_text'),
-    path('bulkclassify/', bulk_classify, name='classify_text')
-]
